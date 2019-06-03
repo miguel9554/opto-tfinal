a2f79ff424a3c371ad276a5304524e8fb8985313
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2019-06-03 09:23:08 -0300

    versión mejorada, queda hacerla andar...

--- read_spectrum/read_spectrum.ino
+++ read_spectrum/read_spectrum.ino
@@ -1,127 +1,117 @@
-/*
- * Macro Definitions
- */
+#include "SerialCommand.h"   /* http://github.com/p-v-o-s/Arduino-SerialCommand */
+
+#if defined(CORE_TEENSY)
+#include <ADC.h> /* https://github.com/pedvide/ADC */
+#else
+#include "elapsedMillis.h"
+#endif
+
+#include "c12880.h"
+
 #define SPEC_TRG         A0
 #define SPEC_ST          A1
-#define SPEC_CLK         A2
+#define SPEC_CLK         10
 #define SPEC_VIDEO       A3
-#define WHITE_LED        A4
-#define LASER_404        A5
 
-#define SPEC_CHANNELS    288 // New Spec Channel
-uint16_t data[SPEC_CHANNELS];
+#if defined(CORE_TEENSY)
+#define FLASH_TRIGGER    12
+IntervalTimer flashTimer;
+#endif
 
+uint16_t data[C12880_NUM_CHANNELS];
+C12880_Class spec(SPEC_TRG,SPEC_ST,SPEC_CLK,SPEC_VIDEO);
+
+SerialCommand sCmd(Serial);// the SerialCommand parser object
+/******************************************************************************/
 void setup(){
-
-  //Set desired pins to OUTPUT
-  pinMode(SPEC_CLK, OUTPUT);
-  pinMode(SPEC_ST, OUTPUT);
-  pinMode(LASER_404, OUTPUT);
-  pinMode(WHITE_LED, OUTPUT);
-
-  digitalWrite(SPEC_CLK, HIGH); // Set SPEC_CLK High
-  digitalWrite(SPEC_ST, LOW); // Set SPEC_ST Low
-
+  #if defined(CORE_TEENSY)
+  pinMode(FLASH_TRIGGER,OUTPUT);
+  digitalWrite(FLASH_TRIGGER,LOW);
+  #endif
   Serial.begin(115200); // Baud Rate set to 115200
-  
-}
-
-/*
- * This functions reads spectrometer data from SPEC_VIDEO
- * Look at the Timing Chart in the Datasheet for more info
- */
-void readSpectrometer(){
-
-  int delayTime = 1; // delay time
-
-  // Start clock cycle and set start pulse to signal start
-  digitalWrite(SPEC_CLK, LOW);
-  delayMicroseconds(delayTime);
-  digitalWrite(SPEC_CLK, HIGH);
-  delayMicroseconds(delayTime);
-  digitalWrite(SPEC_CLK, LOW);
-  digitalWrite(SPEC_ST, HIGH);
-  delayMicroseconds(delayTime);
-
-  //Sample for a period of time
-  for(int i = 0; i < 15; i++){
-
-      digitalWrite(SPEC_CLK, HIGH);
-      delayMicroseconds(delayTime);
-      digitalWrite(SPEC_CLK, LOW);
-      delayMicroseconds(delayTime); 
+  // Setup callbacks for SerialCommand commands
+  sCmd.setDefaultHandler(UNRECOGNIZED_sCmd_default_handler);
+  sCmd.addCommand("SPEC.INTEG", SPEC_INTEG_sCmd_config_handler);     //configures integration time
+  sCmd.addCommand("SPEC.READ?", SPEC_READ_sCmd_query_handler);       //reads out the whole spectrum
+  sCmd.addCommand("SPEC.TIMING?", SPEC_TIMING_sCmd_query_handler);   //reads out timings
+  #if defined(CORE_TEENSY)
+  sCmd.addCommand("FLASH.TIMER_ONCE!", FLASH_TIMER_ONCE_sCmd_action_handler); //set flash to trigger on microsecond delay
+  #endif
+  spec.begin();
  
-  }
-
-  //Set SPEC_ST to low
-  digitalWrite(SPEC_ST, LOW);
-
-  //Sample for a period of time
-  for(int i = 0; i < 85; i++){
-
-      digitalWrite(SPEC_CLK, HIGH);
-      delayMicroseconds(delayTime);
-      digitalWrite(SPEC_CLK, LOW);
-      delayMicroseconds(delayTime); 
-      
-  }
-
-  //One more clock pulse before the actual read
-  digitalWrite(SPEC_CLK, HIGH);
-  delayMicroseconds(delayTime);
-  digitalWrite(SPEC_CLK, LOW);
-  delayMicroseconds(delayTime);
-
-  //Read from SPEC_VIDEO
-  for(int i = 0; i < SPEC_CHANNELS; i++){
-
-      data[i] = analogRead(SPEC_VIDEO);
-      
-      digitalWrite(SPEC_CLK, HIGH);
-      delayMicroseconds(delayTime);
-      digitalWrite(SPEC_CLK, LOW);
-      delayMicroseconds(delayTime);
-        
-  }
-
-  //Set SPEC_ST to high
-  digitalWrite(SPEC_ST, HIGH);
-
-  //Sample for a small amount of time
-  for(int i = 0; i < 7; i++){
-    
-      digitalWrite(SPEC_CLK, HIGH);
-      delayMicroseconds(delayTime);
-      digitalWrite(SPEC_CLK, LOW);
-      delayMicroseconds(delayTime);
-    
-  }
-
-  digitalWrite(SPEC_CLK, HIGH);
-  delayMicroseconds(delayTime);
-  
 }
-
-/*
- * The function below prints out data to the terminal or 
- * processing plot
- */
-void printData(){
-  
-  for (int i = 0; i < SPEC_CHANNELS; i++){
-    
-    Serial.print(data[i]);
-    Serial.print(',');
-    
-  }
-  
-  Serial.print("\n");
-}
-
+/******************************************************************************/
 void loop(){
-   
-  readSpectrometer();
-  printData();
-  delay(10);  
-   
+  int num_bytes = sCmd.readSerial();      // fill the buffer
+  if (num_bytes > 0){
+    sCmd.processCommand();  // process the command
+  }
+  delay(10);
 }
+
+/******************************************************************************/
+// SerialCommand handlers
+
+// Unrecognized command
+void UNRECOGNIZED_sCmd_default_handler(SerialCommand this_sCmd)
+{
+  SerialCommand::CommandInfo command = this_sCmd.getCurrentCommand();
+  this_sCmd.print(F("### Error: command '"));
+  this_sCmd.print(command.name);
+  this_sCmd.print(F("' not recognized ###\n"));
+}
+
+void SPEC_INTEG_sCmd_config_handler(SerialCommand this_sCmd){
+  char *arg = this_sCmd.next();
+  float integ_time;
+  if (arg == NULL){
+    this_sCmd.print(F("### Error: SPEC.INTEG requires 1 argument 'time' (s)\n"));
+  }
+  else{
+    integ_time = atof(arg);
+    spec.set_integration_time(integ_time);
+  }
+}
+
+void SPEC_READ_sCmd_query_handler(SerialCommand this_sCmd){
+  spec.read_into(data);
+  for (int i = 0; i < C12880_NUM_CHANNELS - 1; i++){
+    this_sCmd.print(data[i]);
+    this_sCmd.print(',');
+  }
+  //last value gets special formatting
+  this_sCmd.print(data[C12880_NUM_CHANNELS - 1]);
+  this_sCmd.print("\n");
+}
+
+void SPEC_TIMING_sCmd_query_handler(SerialCommand this_sCmd){
+  for(int i=0; i < 9; i++){
+    this_sCmd.print(spec.get_timing(i));
+    this_sCmd.print(",");
+  }
+  this_sCmd.print(spec.get_timing(9));
+  this_sCmd.print("\n");
+}
+
+#if defined(CORE_TEENSY)
+void FLASH_TIMER_ONCE_sCmd_action_handler(SerialCommand this_sCmd){
+  char *arg = this_sCmd.next();
+  float trig_time;
+  if (arg == NULL){
+    this_sCmd.print(F("### Error: FLASH.TIMER_ONCE! requires 1 argument 'time' (s)\n"));
+  }
+  else{
+    trig_time = atof(arg);
+    trig_time = max(25e-6,trig_time);
+    int trig_micros = (int)(trig_time*1e6);
+    trig_micros = constrain(trig_micros,25,10000000);
+    flashTimer.begin(flash_trigger, trig_micros);
+  }
+}
+
+void flash_trigger(){
+  digitalWrite(FLASH_TRIGGER,HIGH);
+  flashTimer.end();
+  digitalWrite(FLASH_TRIGGER,LOW);
+}
+#endif
--- read_spectrum/read_spectrum.py
+++ read_spectrum/read_spectrum.py
@@ -1,17 +1,32 @@
-import serial
-import matplotlib.pyplot as plt
+import os, glob, serial
+from pylab import *
 
-with serial.Serial('COM4', 115200, timeout=1) as ser:
-	while True:
-		line = str(ser.readline())   # read a '\n' terminated line
-		# print(line.split(',')[:len(line.split(','))-1])
-		try:
-			ints = [int(j) for j in line.split(',')[1:len(line.split(','))-1]]
-			print(ints)
-			if ints:
-				plt.clf()
-				plt.plot(ints)
-				plt.show(block=False)
-				plt.pause(0.05)
-		except Exception:
-			print('a')
+class MicroSpec(object):
+    def __init__(self, port):
+        self._ser = serial.Serial(port,baudrate=115200)
+    def set_integration_time(self, seconds):
+        cmd = "SPEC.INTEG %0.6f\n" % seconds
+        self._ser.write(cmd.encode('utf8'))
+    def read(self):
+        self._ser.write(b"SPEC.READ?\n")
+        
+        sdata = self._ser.readline()
+        sdata = array([int(p) for p in sdata.split(b",")])
+        self._ser.write(b"SPEC.TIMING?\n")
+        tdata = self._ser.readline()
+        tdata = array([int(p) for p in tdata.split(b",")])
+        return (sdata, tdata)
+
+if __name__ == "__main__":
+    DATASET_NAME = "LED Desk Lamp 1ft IT1e-6s Teensy 3.5 120MHz ADC synchronous"
+    mpl.rcParams["savefig.directory"] = os.path.dirname(__file__)
+
+    spec = MicroSpec('COM3')
+    icc = spec.set_integration_time(1e-6)
+    sdata, tdata = spec.read()
+    print("Timings: %r" % (tdata - tdata[0],))
+    
+    plot(sdata)
+    title(DATASET_NAME)
+    show()
+    savetxt("%s.csv" % DATASET_NAME,sdata,delimiter=",")
